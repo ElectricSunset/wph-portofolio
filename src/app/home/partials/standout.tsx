d1bf5e8f6582d36bb730fb32a3cb9cc70d31ab4b
shendy <gunawan.shendy@gmail.com>
2025-04-30 22:54:51 +0700

    add why i Stand Out

--- src/app/home/partials/standout.tsx
+++ src/app/home/partials/standout.tsx
@@ -1,5 +1,7 @@
 import React from 'react';
 
+import { benefits } from '@/constant/standout';
+
 const StandOut: React.FC = () => {
   return (
     <div className='custom-container mt-20 mb-20'>
@@ -12,7 +14,45 @@ const StandOut: React.FC = () => {
           conventional front-end developers
         </span>
       </div>
-      <div></div>
+      <div className='custom-container rounded-3xl bg-neutral-500 p-6'>
+        <table className='text-md w-full font-bold text-neutral-100'>
+          <thead>
+            <tr>
+              <th className='from-secondary-500 to-secondary-400 w-1/3 rounded-l-full bg-gradient-to-r p-3.25'>
+                Skill
+              </th>
+              <th className='from-secondary-400 to-primary-400 w-1/3 bg-gradient-to-r p-3.25'>
+                Me
+              </th>
+              <th className='from-primary-400 to-primary-500 w-1/3 rounded-r-full bg-gradient-to-r p-3.25'>
+                Other
+              </th>
+            </tr>
+          </thead>
+          <tbody>
+            {benefits.map((benefit) => (
+              <tr key={benefit.skill} className='flex'>
+                <th className='p-3.25'>{benefit.skill}</th>
+                <th className='w-1/3 justify-center py-3.25'>
+                  {benefit.me ? (
+                    <div className='flex-center gradient-pink-purple h-7 w-7 rounded-full'></div>
+                  ) : (
+                    <div className='flex-center h-7 w-7 rounded-full bg-neutral-300'></div>
+                  )}
+                </th>
+                <th className='justify-center py-3.25'>
+                  {' '}
+                  {benefit.other ? (
+                    <div className='flex-center gradient-pink-purple h-7 w-7 rounded-full'></div>
+                  ) : (
+                    <div className='flex-center h-7 w-7 rounded-full bg-neutral-300'></div>
+                  )}
+                </th>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </div>
     </div>
   );
 };
---
+++ src/constant/standout.ts
@@ -0,0 +1,15 @@
+type Benefit = {
+  skill: string;
+  me: boolean;
+  other: boolean;
+};
+
+export const benefits: Benefit[] = [
+  { skill: 'React Expert', me: true, other: false },
+  { skill: 'Perfect Pixel', me: true, other: false },
+  { skill: 'Typescript Proficiency', me: true, other: false },
+  { skill: 'Clean, Maintainable Code', me: true, other: false },
+  { skill: 'Responsive Website', me: true, other: false },
+  { skill: 'Performance Optimization', me: true, other: false },
+  { skill: 'UI Design Proficiency (Figma)', me: true, other: false },
+];
